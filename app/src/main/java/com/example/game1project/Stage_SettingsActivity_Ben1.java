6250d771cf6ff80077b7a747c2c260689ce99408
benkwan234 <117450360+benkwan234@users.noreply.github.com>
2023-04-24 22:52:56 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/game1project/Stage_SettingsActivity_Ben1.java
@@ -0,0 +1,35 @@
+package com.example.game1project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    Button button;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent( SettingsActivity.this, MainActivity.class);
+                String username = myIntent.getStringExtra("USER_NAME");
+                startActivity(myIntent);
+                }
+
+
+        });
+    }}
+
+
