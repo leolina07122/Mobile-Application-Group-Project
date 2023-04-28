12878c0a76613302c592350d4c7a7345a5e1731f
peter pang <131172891+999peter-Nagisa@users.noreply.github.com>
2023-04-28 15:58:49 +0800

    Update Stage_Peter1.java

--- app/src/main/java/com/example/game1project/Stage_Peter1.java
+++ app/src/main/java/com/example/game1project/Stage_Peter1.java
@@ -1,19 +1,13 @@
-package com.example.myapplication;
+package com.example.game1project;
 
 
-import android.annotation.SuppressLint;
-import android.content.ClipData;
-import android.content.ClipDescription;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.DragEvent;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,82 +23,77 @@ public class Stage_Peter1 extends AppCompatActivity implements View.OnClickListe
     boolean enter=false;
 
 
-
-    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stage_peter1);
-        loadAnimations();
-        loadUI();
-    }
-
-    private void loadUI() {
-        btnMeat=findViewById(R.id.meat);
+        btnMeat = findViewById(R.id.meat);
         btnMeat.setOnClickListener(this);
-        btnDog=findViewById(R.id.dog);
+        btnDog = findViewById(R.id.dog);
         btnDog.setOnClickListener(this);
-        btnBones=findViewById(R.id.bones);
+        btnBones = findViewById(R.id.bones);
         btnBones.setVisibility(View.INVISIBLE);
         btnBones.setOnClickListener(this);
-        btnKey=findViewById(R.id.key);
+        btnKey = findViewById(R.id.key);
         btnKey.setVisibility(View.INVISIBLE);
         btnKey.setOnClickListener(this);
+        loadAnimations();
+
     }
 
+
+
     private void loadAnimations() {
-        animFadeIn = AnimationUtils.loadAnimation(this,R.anim.fade_in);
-        animFadeOut= AnimationUtils.loadAnimation(this,R.anim.fade_out);
+        animFadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        animFadeOut = AnimationUtils.loadAnimation(this, R.anim.fade_out);
     }
 
     @Override
     public void onClick(View v) {
-        if(v.getId()== R.id.meat) {
-            animFadeOut.setAnimationListener(this);
-            btnMeat.startAnimation(animFadeOut);
-            enter= true;
+        btnKey.setVisibility(View.INVISIBLE);
+        switch (v.getId()) {
+            case R.id.meat: {
+                animFadeOut.setAnimationListener(this);
+                btnMeat.startAnimation(animFadeOut);
 
-            if (enter=true) {
-                animFadeIn.setAnimationListener(this);
-                btnBones.startAnimation(animFadeIn);
-
+                if (!enter) {
+                    animFadeIn.setAnimationListener(this);
+                    btnBones.startAnimation(animFadeIn);
+                }
             }
-
-        }
-        if (v.getId()==R.id.dog)
-        {
-            animFadeOut.setAnimationListener(this);
-            btnBones.startAnimation(animFadeOut);
-            enter=true;
-            if(enter=true){
-                btnKey.setVisibility(View.VISIBLE);
-                btnKey.startAnimation(animFadeIn);
+            case ((R.id.dog) & (R.id.meat)): {
+                animFadeOut.setAnimationListener(this);
+                btnBones.startAnimation(animFadeOut);
+                btnKey.setVisibility(View.INVISIBLE);
                 animFadeOut.setAnimationListener(this);
                 btnDog.startAnimation(animFadeOut);
+                enter = false;
+                btnKey.setVisibility(View.VISIBLE);
+                animFadeIn.setAnimationListener(this);
+                btnKey.startAnimation(animFadeIn);
+                enter = true;
             }
+
+            case (R.id.key)&(R.id.door): {
+                Toast.makeText(this, "You get the key and open the door!", Toast.LENGTH_LONG).show();
+
+            }
+            startActivity(new Intent(Stage_Peter1.this,Stage_Peter2.class));
         }
-        if(v.getId()==R.id.key){
-            btnKey.setOnClickListener(this);
-            if(v.getId()==R.id.door){
-                //link to end game page
+    }
+            @Override
+            public void onAnimationStart (Animation animation){
+
             }
 
+            @Override
+            public void onAnimationEnd (Animation animation){
 
-        }
-    }
+            }
 
-    @Override
-    public void onAnimationStart(Animation animation) {
+            @Override
+            public void onAnimationRepeat (Animation animation){
 
-    }
-
-    @Override
-    public void onAnimationEnd(Animation animation) {
+            }
 
     }
-
-    @Override
-    public void onAnimationRepeat(Animation animation) {
-
-    }
-}
